9e6842a2d8967de7032276240ccb115d0f30a341
Nacho Herrera Ariza <ihariza@gmail.com>
2021-08-23 17:21:40 +0200

    Add theme and custom android style

---
+++ MoviesApp/MoviesApp/Themes/DefaultTheme.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MoviesApp.Themes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DefaultTheme : ResourceDictionary
+    {
+        public DefaultTheme()
+        {
+            InitializeComponent();
+        }
+    }
+}
