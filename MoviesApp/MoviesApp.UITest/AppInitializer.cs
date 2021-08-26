617c96a781b31c651cef5a7db27bb6800a65883c
Nacho Herrera Ariza <ihariza@gmail.com>
2021-08-26 11:43:08 +0200

    Add ui test

---
+++ MoviesApp/MoviesApp.UITest/AppInitializer.cs
@@ -0,0 +1,18 @@
+﻿using Xamarin.UITest;
+
+namespace MoviesApp.UITest
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp.Android.InstalledApp("io.nhariza.moviesapp").StartApp();
+
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+    }
+}
---
+++ MoviesApp/MoviesApp.UITest/Tests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace MoviesApp.UITest
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void AppNameIsDisplayed()
+        {
+            AppResult[] results = app.WaitForElement(c => c.Marked("MoviesApp"));
+            app.Screenshot("Movie List.");
+
+            Assert.IsTrue(results.Any());
+        }
+    }
+}
